b3e605fda81eeb1c7ed2c6b9b6923e8a7f105803
AndreiManache <manacheandrei@gmail.com>
2020-01-30 17:57:15 +0200

    new UPD

--- script.js
+++ script.js
@@ -1,16 +1,31 @@
-var form = document.getElementById('form');
 
 
-form.addEventListener('submit',addRow);
 
-function addRow(e){
-    e.preventDefault();
+function addInput(e){
+e.preventDefault();
+let nume = document.querySelector('.nume').value;
+let telefon = document.querySelector('.telefon').value;
+var str= `
+<tbody>
+<tr>
+    <td class="imp1">${nume}</td>
+    <td class="imp2">${telefon}</td>
+    <td><button class="modifica" onclick="modifyInput(event)">Modifica</button></td>
+    <td><button class="sterge" onclick="deleteInput(event)">Sterge</button></td>
+</tr>
+</tbody>`;
+document.querySelector(".zone2").style.visibility="visible";
+document.querySelector(".table").innerHTML += str;
+}
 
-var newNume = document.querySelector(".nume").value;
-var newTelefon = document.querySelector(".telefon").value;    
+var btn1 = document.querySelector(".modifica");
+btn1.addEventListener('onclick',modifyInput());
 
-var table = document.createElement("table")
-var thead= document.createElement("thead");
-table.appendChild(thead);
+function modifyInput(){
+    
+    document.querySelector('.nume').value = document.querySelector(".imp1").innerText;
+    document.querySelector('.telefon').value = document.querySelector(".imp2").innerText;
 
+    
+    
 }
