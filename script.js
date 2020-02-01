9a5632d968912c9c384c0191ce5a83b8d9a824be
AndreiManache <manacheandrei@gmail.com>
2020-02-01 23:07:17 +0200

    newUpd

--- script.js
+++ script.js
@@ -1,31 +1,55 @@
 
+var list = [];
+var indexEdit;
 
+function draw(){
+    var str = "";
 
-function addInput(e){
-e.preventDefault();
-let nume = document.querySelector('.nume').value;
-let telefon = document.querySelector('.telefon').value;
-var str= `
+    for(let i=0; i<list.length ; i++){
+
+        str+= `
 <tbody>
 <tr>
-    <td class="imp1">${nume}</td>
-    <td class="imp2">${telefon}</td>
-    <td><button class="modifica" onclick="modifyInput(event)">Modifica</button></td>
-    <td><button class="sterge" onclick="deleteInput(event)">Sterge</button></td>
+    <td>${list[i].name}</td>
+    <td>${list[i].number}</td>
+    <td><button class="modifica" onclick="edit(${i})">Modifica</button></td>
+    <td><button class="sterge" onclick="del(${i})">Sterge</button></td>
 </tr>
 </tbody>`;
-document.querySelector(".zone2").style.visibility="visible";
-document.querySelector(".table").innerHTML += str;
-}
 
-var btn1 = document.querySelector(".modifica");
-btn1.addEventListener('onclick',modifyInput());
+document.querySelector("table tbody").innerHTML = str;
 
-function modifyInput(){
-    
-    document.querySelector('.nume').value = document.querySelector(".imp1").innerText;
-    document.querySelector('.telefon').value = document.querySelector(".imp2").innerText;
+    }
+  }
 
-    
-    
-}
+  function del(idx){
+      if(confirm(`ESTI SIGUR CA VREI SA IL STERGI pe ${list[idx].name}?`)){
+          list.splice(idx,1);
+          draw();
+      }
+  }
+
+  function add(event){
+    event.preventDefault();
+
+      var newItem = {
+          name:document.querySelector(".nume").value,
+          number:document.querySelector(".telefon").value
+      };
+
+      if(indexEdit === undefined){
+          list.push(newItem);
+      }else{
+          list[indexEdit]=newItem;
+      }
+      draw();
+  }
+
+  function edit(idx){
+        document.querySelector("name=['nume']").value = list[idx].nume;
+        document.querySelector("name=['telefon']").value = list[idx].telefon;
+        window.indexEdit = idx;
+  }
+
+
+
